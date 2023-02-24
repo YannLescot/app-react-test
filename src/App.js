358782e5301a9ec3ae468d1bcbf04acc6f6d1ac8
YannLescot <yann.lescot@hotmail.com>
2023-02-24 12:17:09 +0100

    premier push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
+import LoginPage from './Components/LoginPage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <AppStyled>
+      <LoginPage/>
+    </AppStyled>
+  )
 }
+
+const AppStyled = styled.div`
+display: flex;
+justify-content: space-evenly;
+  background-color: lavender;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+`;
 
 export default App;
